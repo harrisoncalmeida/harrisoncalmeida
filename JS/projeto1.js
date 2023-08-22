a4ca9d89d907efb031ef3016df85ca2457645c1d
Harrison Almeida <142456909+harrisoncalmeida@users.noreply.github.com>
2023-08-22 10:34:52 -0300

    Alteração

--- JS/projeto1.js
+++ JS/projeto1.js
@@ -1,1 +1,11 @@
- const username = document.getElementById('username')
+ var precoXampu = input (10);
+ var precoSabonete = input (10);
+ var precoXampu = input (10);
+ var precoSabonete = input (10);
+
+ var total = precoXampu * qtdeXampu + precoSabonete * qtdeSabonete;
+
+ if (total > 100) {
+ totaol = total - 10;
+}
+ output(total);
