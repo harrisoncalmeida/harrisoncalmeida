574d7fe4a988140c3e40d37e2ed8c676b4547b8b
Harrison Almeida <142456909+harrisoncalmeida@users.noreply.github.com>
2023-08-24 11:32:23 -0300

    Novas alterações

--- JS/projeto.js
+++ JS/projeto.js
@@ -1,11 +1,2 @@
- var precoXampu = input (10);
- var precoSabonete = input (10);
- var precoXampu = input (10);
- var precoSabonete = input (10);
-
- var total = precoXampu * qtdeXampu + precoSabonete * qtdeSabonete;
-
- if (total > 100) {
- totaol = total - 10;
-}
- output(total);
+output(10);
+output(42);
